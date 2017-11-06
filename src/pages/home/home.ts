4d6212c2caf6dba620e4b64395117e0a7bd98841
Quang Nguyen <quangnd.edu@gmail.com>
2017-11-06 17:18:11 +0700

    Initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { TextToSpeech } from '@ionic-native/text-to-speech';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  locale: string = "en-GB";
+  speed: number = 150;
+  textToSpeak: string;
+  constructor(public navCtrl: NavController, private tts: TextToSpeech) {
+    console.log(this.speed);
+  }
 
-  constructor(public navCtrl: NavController) {
-
+  speak() {
+    this.tts.speak({
+      text: this.textToSpeak,
+      locale: this.locale,
+      rate: this.speed / 100
+    })
+    .then(() => console.log('Success'))
+    .catch((reason: any) => console.log(reason));
   }
 
 }
